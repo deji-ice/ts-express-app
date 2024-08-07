f9fc96d05dc12bd5af0177a4600f7e2ffed9589f
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-07 16:39:07 +0100

    Add login functionality and user model

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -7,6 +7,8 @@ export const login = async (req: Request, res: Response) => {
         return res.status(400).json({ message: `Email and password required` })
     }
 
+    
+
     return res.status(200).json({ message: `Login successful` })
 
 }
---
+++ src/models/userModel.ts
@@ -0,0 +1,35 @@
+import { Document, Model, model, Schema } from "mongoose";
+
+// Define the interface for the User document
+interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    date: Date;
+  }
+
+const userSchema = new Schema<IUser>({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+const User:Model<IUser> = model<IUser>('User', userSchema);
+
+export default User;
