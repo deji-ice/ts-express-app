f5cba759e3d3a530430c2bdc4ce770836982a149
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2024-08-07 17:14:24 +0100

    feat: Add database connection and JSON body parser middleware

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 // src/index.ts
 import express from 'express';
 import authRoute from './routes/authRoute';
+import dbConnection from './db/conn';
 const app = express();
 const port = 3000;
+
+await dbConnection()
+app.use(express.json())
 app.use("/auth", authRoute)
 
 
